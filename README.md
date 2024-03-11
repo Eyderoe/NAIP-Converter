84f0bf68ed9a4277d4a2dd5a89b7c0e6797e60f0
Kipper Eyderoe <k932693932@Outlook.com>
2024-03-11 08:40:57 +0800

    v1.4

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 备注：需要一定动手能力。但代码逻辑比较简单且均包含注释。
 ## PDF extract
 提取数据库编码和航路点编码生成标准文件\
-标准文件可以被读取并转换至不同数据库
+标准文件可以被读取并输出至不同数据库
 ## to MFS Fenix_V1
 目标：微软模拟飞行 Fenix nd.db3\
 支持：所有功能
 ## to X-Plane CIFP
-目标：X-Plane CIFP ZXXX.dat earth_XXX.dat
+目标：X-Plane CIFP ZXXX.dat earth_XXX.dat\
 支持：程序、导航台、航点
--- to X-Plane CIFP/3_航路.py
+++ to X-Plane CIFP/3_航路.py
@@ -1,121 +1,98 @@
-import os.path
+from typing import Set, Tuple, Dict
+
+from colorama import init
 import pandas as pd
-from typing import Union
 from 公共函数 import *
 
 
-class WaypointSystem:
-    def __init__(self):
-        self.base = dict()
+class Segment:
+    def __init__(self, from_point: List[str], to_point: List[str], info: List[str]):
+        self.from_point = from_point  # From
+        self.to_point = to_point  # To
+        self.dir = info[0]  # 方向限制
+        self.alt = info[1]  # 高低空
+        self.limit = (info[2], info[3])  # 航路最低最高高度
+        self.names = info[4].split('-')  # 参与航路
 
-    def add_point(self, point: Waypoint):
-        if point.ident not in self.base:
-            self.base[point.ident] = [point]
-        else:
-            self.base[point.ident].append(point)
+    def get_ident(self) -> str:
+        """返回航段标识"""
+        return "{}*{}".format('*'.join(self.from_point), '*'.join(self.to_point))
 
-    def query(self, submit: Union[str, Waypoint]) -> Union[int, Waypoint]:
-        """
-        在数据库中查询一个航点
-        :param submit: 识别符:str(主用于单机场) 航点类:Waypoint(主用于总数据库)
-        :return: 未找到返回-1 找到返回航点
-        """
-        if type(submit) == str:
-            if submit in self.base:
-                return self.base[submit][0]
-            else:
-                return -1
-        elif type(submit) == Waypoint:
-            if submit.ident in self.base:
-                for iPoint in self.base[submit.ident]:
-                    if iPoint.is_same(submit):
-                        return iPoint
-                else:
-                    return -1
-            else:
-                return -1
-        else:
-            printf("提交表单内容出错", 2)
-
-    def __del__(self):
-        self.base.clear()
-
-
-def location_process(location: str) -> float:
-    first = location[1:3] if 'N' in location else location[1:4]
-    second = location[3:5] if 'N' in location else location[4:6]
-    third = location[5:] if 'N' in location else location[6:]
-    return round(int(first) / 1 + int(second) / 60 + int(third) / 3600, 9)
-
-
-class Info:  # 一个航段的基础信息
-    def __init__(self, information: List[str]):
-        self.base = '*'.join(information[:4])  # 基础信息
-        self.airway = information[4]  # 航路
-
-
-class AirwaySystem:  # 一堆航路
-    def __init__(self):
-        self.base = dict()  # [from/to]:List[Info]
-
-    def add_exist_record(self, record: str):
-        """将xp数据库里面的东西写进去"""
-        record = record.split()
-        head = '*'.join(record[:6])
-        if head not in self.base:
-            self.base[head] = [Info(record[6:])]
-        else:
-            self.base[head].append(Info(record[6:]))
-
-    def add_naip_record(self, point1: Waypoint, point2: Waypoint, ident: str):
-        def in_database(head: str, awy: str) -> int:
-            if head not in self.base:
-                return 1  # 航段不存在
-            else:
-                for iInfo in self.base[head]:
-                    if awy in iInfo.airway.split('-'):
-                        return 2  # 航段存在 航线存在
-                else:
-                    return 3  # 航段存在 航线不存在
-
-        from_point = "{}*{}*{}".format(point1.ident, point1.area, point1.cat)
-        to_point = "{}*{}*{}".format(point2.ident, point2.area, point2.cat)
-        forward = from_point + '*' + to_point
-        backward = to_point + '*' + from_point
-        # 存在
-        forward_result = in_database(forward, ident)
-        backward_result = in_database(backward, ident)
-        if (forward_result == 2) or (backward_result == 2):
-            return
-        # 不存在 先这样写看能不能读 从这里往后可以重构 好的 重构次数：1
-        empty_seg = True if forward_result + backward_result < 3 else False
-        if forward_result == 3:
-            master = forward
-        elif backward_result == 3:
-            master = backward
-        else:
-            master = forward
-
-        if empty_seg:
-            self.base[master] = [Info(['N', '1', '0', '0', ident])]
-        else:
-            for iInfoLoc in range(len(self.base[master])):  # 唉 python总是搞不懂这一坨
-                if self.base[master][iInfoLoc].base.startswith('N'):  # 假如这是一个双向线段
-                    self.base[master][iInfoLoc].airway += ("-{}".format(ident))
-                    break
-            else:  # 没有找到双向线段
-                self.base[master].append(Info(['N', '1', '0', '0', ident]))
+    def get_point_ident(self) -> Tuple[str, str]:
+        """返回始终航点名称"""
+        return self.from_point[0], self.to_point[0]
 
     def output(self) -> str:
-        text = []
-        keys = self.base.keys()
-        for iKey in keys:
-            for iInfo in self.base[iKey]:
-                i_key = iKey
-                i_key += ('*' + iInfo.base)
-                i_key += ('*' + iInfo.airway)
-                text.append(i_key.replace('*', ' '))
-        return '\n'.join(text)
+        info = self.from_point + self.to_point + [self.dir, self.alt, self.limit[0], self.limit[1],
+                                                  '-'.join(self.names)]
+        return ' '.join(info)
+
+
+class Airways:
+    def __init__(self):
+        self.base: List[Segment] = []
+        self.index: Dict[str, List[int]] = dict()  # 根据from to索引
+        self.name_index: Dict[str, List[int]] = dict()  # 根据航路名称索引
+
+    def output(self) -> List[str]:
+        all_seg = []
+        for iSeg in self.base:
+            if len(iSeg.names) > 0:
+                all_seg.append(iSeg.output())
+        return all_seg
+
+    def add_segment(self, seg: Segment):
+        """
+        添加一个航段至系统
+        :param seg: 航段
+        :param ignore: 是否直接添加
+        """
+        ident = seg.get_ident()
+        if ident in self.index:
+            self.index[ident].append(len(self.base))
+        else:
+            self.index[ident] = [len(self.base)]
+        for iName in seg.names:
+            if iName in self.name_index:
+                self.name_index[iName].append(len(self.base))
+            else:
+                self.name_index[iName] = [len(self.base)]
+        self.base.append(seg)
+
+    def try_add_seg(self, point1: Waypoint, point2: Waypoint, awy_name: str):
+        """添加两个点进去"""
+        ident = [point1.ident, point1.area, str(point1.cat), point2.ident, point2.area, str(point2.cat)]
+        forward = '*'.join(ident[:3] + ident[3:])
+        backward = '*'.join(ident[3:] + ident[:3])
+        # 前向通路有 就写前向
+        if forward in self.index:
+            for jLoc in self.index[forward]:
+                if self.base[jLoc].dir == 'N':
+                    self.base[jLoc].names.append(awy_name)
+                    return
+        # 反馈回路有 就写反馈
+        if backward in self.index:
+            for jLoc in self.index[backward]:
+                if self.base[jLoc].dir == 'N':
+                    self.base[jLoc].names.append(awy_name)
+                    return
+        # 都没有 就按前向写
+        self.add_segment(Segment(forward.split('*')[:3], forward.split('*')[3:], ['N', '1', '0', '0', awy_name]))
+
+    def del_segment(self, name: str, point_set: Set[str]):
+        """
+        删除包含在point_set中且航路名为name的航段
+        :param name: 航路名
+        :param point_set: 航点集合
+        """
+        # 对于单一航路 判断航点名称应该就行了 删除不删除本体 不然索引就要重写
+        if name not in self.name_index:
+            return
+        else:
+            for iLoc in self.name_index[name]:
+                from_point, to_point = self.base[iLoc].get_point_ident()
+                if (from_point in point_set) and (to_point in point_set):
+                    self.base[iLoc].names.remove(name)
 
 
 def read_points(folder: str):
@@ -124,70 +101,102 @@ def read_points(folder: str):
     :param folder: xp数据文件夹路径
     """
     global waypoints
-    with open(os.path.join(folder, "earth_fix.dat"), 'r', encoding="utf-8") as f:
+    # earth_awy中 11航点 2无向信标 3甚高频
+    with open(os.path.join(folder, "earth_fix.dat"), 'r', encoding="utf-8") as f:  # 添加航点
         f = f.readlines()
         f = f[3:-1]
         for iLine in f:
             i_point = iLine.split()
             i_point = Waypoint(float(i_point[0]), float(i_point[1]), i_point[2], 11, i_point[3], i_point[4])
             waypoints.add_point(i_point)
-    with open(os.path.join(folder, "earth_nav.dat"), 'r', encoding="utf-8") as f:
+    with open(os.path.join(folder, "earth_nav.dat"), 'r', encoding="utf-8") as f:  # 添加导航台
         f = f.readlines()
-        f = [i for i in f if not empty(i, 4)]
         f = f[3:-1]
         for iLine in f:
             i_point = iLine.split()
-            if "NDB" in i_point[11]:
+            if i_point[0] == '2':  # NDB台
                 i_point = Waypoint(float(i_point[1]), float(i_point[2]), i_point[7], 2, i_point[8], i_point[9])
-            else:
+            elif (i_point[0] == '3') or (i_point[0] == "13"):  # VHF台
                 i_point = Waypoint(float(i_point[1]), float(i_point[2]), i_point[7], 3, i_point[8], i_point[9])
+            else:
+                continue
             waypoints.add_point(i_point)
 
 
+def spawn_points(loc: int, table: pd.DataFrame) -> Tuple[Waypoint, Waypoint]:
+    """生成from和to两点"""
+
+    def location_process(location: str) -> float:
+        """计算经纬度"""
+        first = location[1:3] if 'N' in location else location[1:4]
+        second = location[3:5] if 'N' in location else location[4:6]
+        third = location[5:] if 'N' in location else location[6:]
+        return round(int(first) / 1 + int(second) / 60 + int(third) / 3600, 9)
+
+    from_ident = table.iloc[loc, 5]
+    if len(from_ident) > 5:
+        from_ident = from_ident[:5]
+    from_la = table.iloc[loc, 9]
+    from_long = table.iloc[loc, 8]
+    from_point = Waypoint(location_process(from_la), location_process(from_long), from_ident, -1)
+    to_ident = table.iloc[loc, 14]
+    if len(to_ident) > 5:
+        to_ident = to_ident[:5]
+    to_la = table.iloc[loc, 18]
+    to_long = table.iloc[loc, 17]
+    to_point = Waypoint(location_process(to_la), location_process(to_long), to_ident, -1)
+    return from_point, to_point
+
+
+counter = 0
+init(autoreset=True)
 xplanePath = r"E:\steampower\steamapps\common\X-Plane 11\Custom Data"
 csvPath = r"F:\航图\中国民航国内航空资料汇编 NAIP 2312\DataFile\RTE_SEG.csv"
-# 建立xp航点库
+# 建立航点库
 waypoints = WaypointSystem()
 read_points(xplanePath)
-# xp原始航路
-airwayFile = open(os.path.join(xplanePath, "earth_awy.dat"), 'r', encoding="utf-8")
-airwayText = airwayFile.readlines()
-airwayFile.close()
-airwayText = airwayText[3:-1]
-airways = AirwaySystem()
-for iLine in airwayText:
-    airways.add_exist_record(iLine)
-# naip航路
-df = pd.read_csv(csvPath, encoding="gbk", dtype=str)
-for iLoc in range(df.shape[0]):
-    fromPoint = Waypoint(location_process(df.iloc[iLoc, 9]), location_process(df.iloc[iLoc, 8]), df.iloc[iLoc, 5], -1)
-    toPoint = Waypoint(location_process(df.iloc[iLoc, 18]), location_process(df.iloc[iLoc, 17]), df.iloc[iLoc, 14], -1)
-    airwayIdent = df.iloc[iLoc, 36]
-    # 跳过情况
-    if airwayIdent.startswith("XX"):  # 好像是直升机目视航向
+printf("航点库建立完毕", 3)
+# 添加已有航段
+airway = Airways()
+with open(os.path.join(xplanePath, "earth_awy.dat"), 'r', encoding="utf-8") as f:
+    f = f.readlines()
+    f = f[3:-1]
+    for iLine in f:
+        iLine = iLine.split()
+        iSegment = Segment(iLine[0:3], iLine[3:6], iLine[6:])
+        airway.add_segment(iSegment)
+printf("已有航段添加完毕", 3)
+# 删除国区航路 丢失所有信息 悲
+naip = pd.read_csv(csvPath, encoding="gbk")
+airwayNames = set(naip["TXT_DESIG"].tolist())
+for iWayName in airwayNames:
+    result = naip[naip["TXT_DESIG"] == iWayName]
+    points = set(result["CODE_POINT_START"].tolist() + result["CODE_POINT_END"].tolist())
+    airway.del_segment(iWayName, points)
+printf("线段删除完毕", 3)
+# 重新添加进去
+for iWayName in airwayNames:
+    result = naip[naip["TXT_DESIG"] == iWayName]
+    if iWayName.startswith("XX"):
         continue
-    if ('*' in fromPoint.ident) or ('*' in toPoint.ident):  # M771 ****
-        continue
-    # 修正情况
-    airwayIdent = airwayIdent.replace('-', '')  # FANS-1
-    if len(fromPoint.ident) > 5:  # AIWD50/CH
-        fromPoint.ident = fromPoint.ident[:5]
-    if len(toPoint.ident) > 5:
-        toPoint.ident = toPoint.ident[:5]
-    # 写入数据库
-    fromPoint = waypoints.query(fromPoint)
-    toPoint = waypoints.query(toPoint)
-    if (fromPoint == -1) or (toPoint == -1):
-        printf("未知点出现在{}行".format(iLoc), 2)
-    airways.add_naip_record(fromPoint, toPoint, airwayIdent)
-# 写入文件
-addon = airways.output()
-airwayFile = open(os.path.join(xplanePath, "earth_awy.dat"), 'r', encoding="utf-8")
-airwayText = airwayFile.readlines()
-airwayFile.close()
-airwayText = airwayText[:3]
-airwayText += addon
-airwayText.append("\n99\n")
-airwayFile = open(r".\output\awy.dat", 'w', encoding="utf-8")
-airwayFile.writelines(airwayText)
-airwayFile.close()
+    if '-' in iWayName:
+        iWayName = iWayName.replace('-', '')
+    for iLoc in range(result.shape[0]):
+        fromPoint, toPoint = spawn_points(iLoc, result)
+        fromPoint = waypoints.query(fromPoint)
+        toPoint = waypoints.query(toPoint)
+        if (fromPoint == -1) or (toPoint == -1):
+            printf("出现未知点 {}".format(iWayName), 2)
+            continue
+        airway.try_add_seg(fromPoint, toPoint, iWayName)
+printf("线段添加完毕", 3)
+# 导出
+with open(r"E:\steampower\steamapps\common\X-Plane 11\Custom Data\earth_awy.dat", 'r', encoding="utf-8") as f:
+    f = f.readlines()
+    f = f[:3]
+segments = airway.output()
+segments = [i + '\n' for i in segments]
+with open(r".\output\awy.dat", 'w', encoding="utf-8") as file:
+    file.writelines(f)
+    file.writelines(segments)
+    file.write("99\n")
